7294ccf3a5dc4f50dcb4e4ea0382c9b058e49c63
gintare-ces <gintare.cesiuliene@gmail.com>
2023-03-27 10:50:39 +0300

    efektas vyksta priklausomai nuo count, keicia colr

--- src/components/ui/Counter.jsx
+++ src/components/ui/Counter.jsx
@@ -2,19 +2,48 @@ import React, { useState, useEffect } from "react";
 import Card from "./card/Card";
 // susikuriam komponenta
 function Counter() {
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
+    const [colorClass, setColorClass] = useState('black')
+    const style = {
+    color: colorClass,
+  }
+//   jei counter daugiau uz 5 spalva taps zalia, jei maziau juoda
+//   style.color = count > 5 ? 'green' : 'black'
   setTimeout(() => {
     document.title = "pakeista";
   }, 3000);
 
 
   function increase() {
-    setCount(count + 1)
+    setCount((prevCount) => prevCount + 1)
   }
+
+//   efektas pasileidzia tik susikurus komponentui
+  useEffect(() => {
+    setTimeout(() => {
+        // setCount((prevCount) => prevCount + 1)
+    }, 5000)
+  }, [])
+
+  useEffect(() => {
+    console.log('pasikeite count', count);
+    switch(true) {
+        case count < 5:
+            setColorClass( 'green')
+            break;
+        case count < 7:
+            setColorClass( 'blue')
+            break;
+        case count < 11:
+            setColorClass( 'red')
+            break;
+    }
+  }, [count])
+  
 
   return (
   <Card center>
-    <h2>{count}</h2>
+    <h2 style={style}>{count}</h2>
     <button onClick={increase}>Plus</button>
   </Card>)
 }
